45789b781616f3887c11503aa6b0fdd8700fdcbe
Stanca Pop <stanca.pop@analog.com>
2022-05-06 13:09:19 +0300

    ad7616_sdz: Cosmetic changes

--- projects/ad7616_sdz/common/ad7616_bd.tcl
+++ projects/ad7616_sdz/common/ad7616_bd.tcl
@@ -4,23 +4,20 @@
 set SI_OR_PI $ad_project_params(SI_OR_PI)
 puts "build parameters: SI_OR_PI: $SI_OR_PI"
 
-# data interfaces
-
-if {$SI_OR_PI == 1} {
-  create_bd_port -dir O -from 15 -to 0 rx_db_o
-  create_bd_port -dir I -from 15 -to 0 rx_db_i
-  create_bd_port -dir O rx_db_t
-  create_bd_port -dir O rx_rd_n
-  create_bd_port -dir O rx_wr_n
-  create_bd_port -dir O rx_cs_n
-}
-
 # control lines
 
 create_bd_port -dir O rx_cnvst
 create_bd_port -dir I rx_busy
 
 # instantiation
+
+# dma
+
+ad_ip_instance axi_dmac axi_ad7616_dma
+ad_ip_parameter axi_ad7616_dma CONFIG.DMA_TYPE_DEST 0
+ad_ip_parameter axi_ad7616_dma CONFIG.CYCLIC 0
+ad_ip_parameter axi_ad7616_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter axi_ad7616_dma CONFIG.DMA_DATA_WIDTH_DEST 64
 
 ad_ip_instance axi_pwm_gen ad7616_pwm_gen
 ad_ip_parameter ad7616_pwm_gen CONFIG.PULSE_0_PERIOD 100
@@ -53,42 +50,13 @@ if {$SI_OR_PI == 0} {
 
   spi_engine_create $hier_spi_engine $data_width $async_spi_clk $num_cs $num_sdi $sdi_delay
 
-  ad_connect busy_sync/out_resetn $hier_spi_engine/axi_regmap/spi_resetn
-  ad_connect busy_capture/signal_out $hier_spi_engine/offload/trigger
-
-} else {
-  ad_ip_instance axi_ad7616 axi_ad7616
-  ad_connect busy_capture/signal_out axi_ad7616/rx_trigger
-  ad_connect busy_sync/out_resetn sys_cpu_resetn 
-}
-
-# dma
-
-ad_ip_instance axi_dmac axi_ad7616_dma
-ad_ip_parameter axi_ad7616_dma CONFIG.DMA_TYPE_DEST 0
-ad_ip_parameter axi_ad7616_dma CONFIG.CYCLIC 0
-ad_ip_parameter axi_ad7616_dma CONFIG.DMA_2D_TRANSFER 0
-ad_ip_parameter axi_ad7616_dma CONFIG.DMA_DATA_WIDTH_DEST 64
-
-if {$SI_OR_PI == 0} {
   ad_ip_parameter axi_ad7616_dma CONFIG.DMA_DATA_WIDTH_SRC 32
   ad_ip_parameter axi_ad7616_dma CONFIG.DMA_TYPE_SRC 1
   ad_ip_parameter axi_ad7616_dma CONFIG.SYNC_TRANSFER_START 0
   ad_ip_parameter axi_ad7616_dma CONFIG.AXI_SLICE_SRC 0
   ad_ip_parameter axi_ad7616_dma CONFIG.AXI_SLICE_DEST 1
-} else {
-  ad_ip_parameter axi_ad7616_dma CONFIG.DMA_DATA_WIDTH_SRC 16
-  ad_ip_parameter axi_ad7616_dma CONFIG.DMA_TYPE_SRC 2
-}
 
-
-# interface connections
-
-ad_connect  ad7616_pwm_gen/pwm_0 rx_cnvst
-ad_connect  $sys_cpu_clk ad7616_pwm_gen/s_axi_aclk
-ad_connect  sys_cpu_resetn ad7616_pwm_gen/s_axi_aresetn
-
-if {$SI_OR_PI == 0} {
+  # interface connections
 
   ad_connect  sys_cpu_clk $hier_spi_engine/clk
   ad_connect  sys_cpu_resetn $hier_spi_engine/resetn
@@ -97,14 +65,38 @@ if {$SI_OR_PI == 0} {
   ad_connect  sys_cpu_clk axi_ad7616_dma/s_axis_aclk
   ad_connect  axi_ad7616_dma/s_axis $hier_spi_engine/m_axis_sample
 
+  ad_connect busy_sync/out_resetn $hier_spi_engine/axi_regmap/spi_resetn
+  ad_connect busy_capture/signal_out $hier_spi_engine/offload/trigger
+
+  # interconnect
+
+  ad_cpu_interconnect  0x44A00000 $hier_spi_engine/axi_regmap
+
+  # interrupts
+
+  ad_cpu_interrupt ps-14 mb-14 /$hier_spi_engine/irq
 } else {
+  # data interfaces
+
+  create_bd_port -dir O -from 15 -to 0 rx_db_o
+  create_bd_port -dir I -from 15 -to 0 rx_db_i
+  create_bd_port -dir O rx_db_t
+  create_bd_port -dir O rx_rd_n
+  create_bd_port -dir O rx_wr_n
+  create_bd_port -dir O rx_cs_n
+
+  ad_ip_parameter axi_ad7616_dma CONFIG.DMA_DATA_WIDTH_SRC 16
+  ad_ip_parameter axi_ad7616_dma CONFIG.DMA_TYPE_SRC 2
+
+  ad_ip_instance axi_ad7616 axi_ad7616
+
+  # interface connections
 
   ad_connect  rx_db_o axi_ad7616/rx_db_o
   ad_connect  rx_db_i axi_ad7616/rx_db_i
   ad_connect  rx_db_t axi_ad7616/rx_db_t
   ad_connect  rx_rd_n axi_ad7616/rx_rd_n
   ad_connect  rx_wr_n axi_ad7616/rx_wr_n
-
   ad_connect  rx_cs_n axi_ad7616/rx_cs_n
 
   ad_connect  sys_cpu_clk axi_ad7616_dma/fifo_wr_clk
@@ -112,20 +104,29 @@ if {$SI_OR_PI == 0} {
   ad_connect  axi_ad7616/adc_data axi_ad7616_dma/fifo_wr_din
   ad_connect  axi_ad7616/adc_sync axi_ad7616_dma/fifo_wr_sync
 
+  ad_connect busy_capture/signal_out axi_ad7616/rx_trigger
+  ad_connect busy_sync/out_resetn sys_cpu_resetn
+  # interconnect
+
+  ad_cpu_interconnect  0x44A80000 axi_ad7616
+
+  # interrupts
+
+  ad_cpu_interrupt ps-12 mb-13 axi_ad7616/irq
 }
 
-ad_connect sys_cpu_clk axi_ad7616_dma/s_axi_aclk
-ad_connect sys_cpu_resetn axi_ad7616_dma/m_dest_axi_aresetn
+# interface connections
+
+ad_connect  ad7616_pwm_gen/pwm_0 rx_cnvst
+ad_connect  $sys_cpu_clk ad7616_pwm_gen/s_axi_aclk
+ad_connect  sys_cpu_resetn ad7616_pwm_gen/s_axi_aresetn
+ad_connect  sys_cpu_clk axi_ad7616_dma/s_axi_aclk
+ad_connect  sys_cpu_resetn axi_ad7616_dma/m_dest_axi_aresetn
 
 # interconnect
 
 ad_cpu_interconnect  0x44A30000 axi_ad7616_dma
 ad_cpu_interconnect  0x44B00000 ad7616_pwm_gen
-if {$SI_OR_PI == 0} {
-  ad_cpu_interconnect  0x44A00000 $hier_spi_engine/axi_regmap
-} else {
-  ad_cpu_interconnect  0x44A80000 axi_ad7616
-}
 
 # memory interconnect
 
@@ -135,8 +136,3 @@ ad_mem_hp1_interconnect sys_cpu_clk axi_ad7616_dma/m_dest_axi
 # interrupts
 
 ad_cpu_interrupt ps-13 mb-12 axi_ad7616_dma/irq
-if {$SI_OR_PI == 0} {
-  ad_cpu_interrupt ps-14 mb-14 /$hier_spi_engine/irq
-} else {
-  ad_cpu_interrupt ps-12 mb-13 axi_ad7616/irq
-}
